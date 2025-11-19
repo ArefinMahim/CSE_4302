242411e1e0990d1ab9d4823b2d95805207e5abdd
ArefinMahim <arefinmahim14@gmail.com>
2025-11-19 11:16:17 +0600

    Lab2 file is created

---
+++ OOP/Lab2/136_task1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    char e,f;
+
+    cout<<"Enter first fraction:: ";
+    cin>>a>>e>>b;
+
+    cout<<"Enter second fraction: ";
+    cin>>c>>f>>d;
+
+    cout<<"Sum: "<<(a*d)+(b*c)<<e<<(b*d);
+
+
+
+    return 0;
+}
